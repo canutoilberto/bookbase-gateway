4f7d61dbbc51a2cf31145b54b9abd9c25baba8f3
canutoilberto <canutoilberto@gmail.com>
2025-04-24 10:19:35 -0300

    [feat] route params first implementation

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,14 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { useEffect } from "react";
 
 const NotFound = () => {
   const location = useLocation();
+
+  const { token } = useParams();
+
+  useEffect(() => {
+    console.log(token);
+  }, [token]);
 
   useEffect(() => {
     console.error(
@@ -15,9 +21,11 @@ const NotFound = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
+        <p className="text-xl text-gray-600 mb-4">
+          Oops! Página não encontrada
+        </p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+          Volte ao início
         </a>
       </div>
     </div>
